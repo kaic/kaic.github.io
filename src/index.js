07a990c4d1da44e9c3e26d9d474c00978f7306ac
Kaic <kaicbento@outlook.com>
2018-03-10 01:14:48 -0300

    now using font awesome

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import 'popper.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import './style.scss';
+
+import fontawesome from '@fortawesome/fontawesome';
+import brands from '@fortawesome/fontawesome-free-brands'
+
+fontawesome.library.add(brands.faLinkedinIn);
+fontawesome.library.add(brands.faGithub);
