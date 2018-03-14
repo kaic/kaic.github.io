fc760b682f3c9356001903f58d2cdcaaa223f284
Kaic <kaicbento@outlook.com>
2018-03-13 22:38:13 -0300

    service worker init

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
 import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
 
 fontawesome.library.add(faGithub, faLinkedinIn, faEnvelopeSquare);
+
+if ('serviceWorker' in navigator) {
+    console.log('Service worker is registrating.');
+    navigator.serviceWorker.register('./service-worker.js').then(function () {
+        console.log('service worker registration complete.');
+    }, function () {
+        console.log('sservice worker registration failure.');
+    });
+} else {
+    console.log('service worker is not supported.');
+}
