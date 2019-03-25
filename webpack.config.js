0dd4327024b63378b54613db8a54de8b4c8b73cc
Kaic Bento <kaicbento@outlook.com>
2019-03-25 19:43:24 -0300

    now using title atribute

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,8 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/index.html",
-            filename: "./index.html"
+            filename: "./index.html",
+            title: "Kaic Bento - Software Developer"
         })
     ]
 };
