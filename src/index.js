b53080192a44ee29facb220725107044b48108db
Kaic Bento <kaicbento@outlook.com>
2019-03-25 19:46:14 -0300

    importing and using new font awsome icons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import './style.scss';
 
 import fontawesome from '@fortawesome/fontawesome';
+import faMapPin from '@fortawesome/fontawesome-free-solid/faMapPin';
+import faCircle from '@fortawesome/fontawesome-free-solid/faCircle';
 import faEnvelopeSquare from '@fortawesome/fontawesome-free-solid/faEnvelopeSquare';
 import faLinkedin from '@fortawesome/fontawesome-free-brands/faLinkedin';
 import faGithubSquare from '@fortawesome/fontawesome-free-brands/faGithubSquare';
 import faGithubAlt from '@fortawesome/fontawesome-free-brands/faGithubAlt';
 import faMedium from '@fortawesome/fontawesome-free-brands/faMedium';
+import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
 
-fontawesome.library.add(faGithubSquare, faLinkedin, faEnvelopeSquare, faMedium, faGithubAlt);
+fontawesome.library.add(faGithubSquare, faEnvelope, faLinkedin, faEnvelopeSquare, faMedium, faMapPin, faCircle);
 
 window.addEventListener("load",function() {
     setTimeout(function(){
@@ -15,7 +18,7 @@ window.addEventListener("load",function() {
     }, 0);
 })
 
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
     console.log('Service worker is registrating.');
     navigator.serviceWorker.register('service-worker.js').then(function () {
         console.log('service worker registration complete.');
@@ -24,4 +27,4 @@ if ('serviceWorker' in navigator) {
     });
 } else {
     console.log('service worker is not supported.');
-}
+}*/
