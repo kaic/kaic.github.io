209e48b408c8278b92fa0411ef2845a1fefc137a
Kaic Bento <kaicbento@outlook.com>
2019-10-07 20:30:37 -0300

    remove font awesome

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import './style/main.scss'
 
-import fontawesome from '@fortawesome/fontawesome'
-import faLinkedin from '@fortawesome/fontawesome-free-brands/faLinkedin'
-import faGithubSquare from '@fortawesome/fontawesome-free-brands/faGithubSquare'
-import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope'
-import faFilePdf from '@fortawesome/fontawesome-free-solid/faFilePdf'
-import faArrowUp from '@fortawesome/fontawesome-free-solid/faArrowUp'
-
-const myBirthdate = new Date(1998, 6, 12)
+const MY_BIRTHDATE = new Date(1998, 6, 12)
 
 const calculateMyAge = birthdate => {
   var diffMs = Date.now() - birthdate.getTime()
@@ -16,21 +9,7 @@ const calculateMyAge = birthdate => {
   return Math.abs(ageDate.getUTCFullYear() - 1970)
 }
 
-document.getElementById('age').innerText = calculateMyAge(myBirthdate)
-
-const goTopBtn = document.querySelector('.scroll-to-top-btn')
-
-const trackScroll = () => {
-  const scrolled = window.pageYOffset
-  const coords = document.documentElement.clientHeight
-
-  if (scrolled > coords) {
-    goTopBtn.classList.add('scroll-to-top-btn-show')
-  }
-  if (scrolled < coords) {
-    goTopBtn.classList.remove('scroll-to-top-btn-show')
-  }
-}
+document.getElementById('age').innerText = calculateMyAge(MY_BIRTHDATE)
 
 const showCollapse = (divName, event) => {
   const content = document.getElementById(divName)
@@ -63,21 +42,11 @@ const closeCollapses = () => {
 
 window.showCollapse = showCollapse
 
-window.addEventListener('scroll', trackScroll)
-
 window.addEventListener('load', () => {
   setTimeout(() => {
     window.scrollTo(0, 1)
   }, 0)
 })
-
-fontawesome.library.add(
-  faGithubSquare,
-  faFilePdf,
-  faArrowUp,
-  faEnvelope,
-  faLinkedin
-)
 
 if (process.env.NODE_ENV === 'production') {
   if ('serviceWorker' in navigator) {
