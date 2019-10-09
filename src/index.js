41008fec7b66bf2b4ff6e96c65fe66c6ab4e5294
Kaic Bento <kaicbento@outlook.com>
2019-10-09 11:24:47 -0300

    load skillset div dynamically in js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,115 @@ import './style/main.scss'
 
 const MY_BIRTHDATE = new Date(1998, 6, 12)
 
+const skillsets =
+  [
+    {
+      name: 'Linguagens',
+      skills: [{
+        name: "JavaScript",
+        icon: "javascript"
+      }]
+    },
+    {
+      name: 'Backend',
+      skills: [{
+        name: "NodeJS - Express, HapiJS",
+        icon: "nodejs"
+      }, {
+        name: "RESTful APIs",
+        icon: "api-settings"
+      },
+      {
+        name: "Microservices",
+        icon: "cloud-connection"
+      }]
+    },
+    {
+      name: 'Frontend',
+      skills: [{
+        name: "HTML 5",
+        icon: "html-5"
+      },
+      {
+        name: "CSS 3",
+        icon: "css3"
+      },
+      {
+        name: "Bootstrap",
+        icon: "bootstrap"
+      },
+      {
+        name: "Angular",
+        icon: "angularjs"
+      },
+      {
+        name: "SASS",
+        icon: "sass"
+      },
+      {
+        name: "Ionic",
+        icon: "ionic"
+      }]
+    },
+    {
+      name: 'Data',
+      skills: [
+        {
+          name: "SQL",
+          icon: "sql"
+        },
+        {
+          name: "MySQL",
+          icon: "mysql"
+        },
+        {
+          name: "PostgreSQL",
+          icon: "postgreesql"
+        },
+        {
+          name: "MongoDB",
+          icon: "mongodb"
+        }]
+    },
+    {
+      name: 'Ferramental',
+      skills: [
+        {
+          name: "NPM",
+          icon: "npm"
+        },
+        {
+          name: "Jenkins",
+          icon: "jenkins"
+        },
+        {
+          name: "Git",
+          icon: "git"
+        },
+        {
+          name: "Linux",
+          icon: "linux"
+        },
+        {
+          name: "Bash",
+          icon: "console"
+        },
+        {
+          name: "Webpack",
+          icon: "webpack"
+        },
+        {
+          name: "Docker",
+          icon: "docker"
+        },
+        {
+          name: "Scrum",
+          icon: "sprint-iteration"
+        }
+      ]
+    }
+  ]
+
 const calculateMyAge = birthdate => {
   var diffMs = Date.now() - birthdate.getTime()
   var ageDate = new Date(diffMs)
@@ -10,6 +119,26 @@ const calculateMyAge = birthdate => {
 }
 
 document.getElementById('age').innerText = calculateMyAge(MY_BIRTHDATE)
+
+const skillsetHtml = `<div class="col-lg-8 col-md-10">
+${skillsets.map(skillset => {
+  return `
+  <div class="skills-container">
+  <h3>${skillset.name}</h3>
+  ${skillset.skills.map(skill => {
+    return `<div class="skill">
+    <i class="icon-${skill.icon}">
+    <svg>
+    <use xlink:href="./assets/icons/icons.svg#icon-${skill.icon}"></use>
+    </svg>
+    </i> <span class="justify-content-center text-center">${skill.name}</span></div>`
+  }).join('')}
+</div>`
+}).join('')}
+<hr />
+</div>`
+document.getElementById('skillsets').insertAdjacentHTML('beforeend', skillsetHtml)
+
 
 const showCollapse = (divName, event) => {
   const content = document.getElementById(divName)
