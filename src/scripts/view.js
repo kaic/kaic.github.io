eaec6ba435160ca3e9f4746f13720bd418cbad2c
Kaic <kaicbento@outlook.com>
2022-09-03 18:40:46 -0300

    feat: change about me text

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -2,7 +2,7 @@ import { t } from './i18n'
 import { MY_AGE } from './date'
 
 const headerTextElements = [{ id: 't-hello', args: null }, { id: 't-slogan', args: null }, { id: 't-location-1', args: null }, { id: 't-location-2', args: null }, { id: 't-about-link', args: null }, { id: 't-career-link', args: null }, { id: 't-contact-link', args: null },]
-const aboutTextElements = [{ id: 't-about-title', args: null }, { id: 't-about-p1', args: { MY_AGE } }, { id: 't-about-p2', args: null }, { id: 't-about-p3', args: null }, { id: 't-about-p4', args: null }, { id: 't-about-p5', args: null }]
+const aboutTextElements = [{ id: 't-about-title', args: null }, { id: 't-about-p1', args: { MY_AGE } }, { id: 't-about-p2', args: null }, { id: 't-about-p3', args: null }, { id: 't-about-p4', args: null }, { id: 't-about-p5', args: null }, { id: 't-about-p6', args: null }]
 const careerTextElements = [{ id: 't-career-title', args: null }]
 const contactTextElements = [{ id: 't-contact-title', args: null }, { id: 't-contact-p1', args: null }]
 const footerTextElements = [{ id: 't-footer-p1', args: null }, { id: 't-footer-sourcecode', args: null }, { id: 't-footer-p2', args: null }]
