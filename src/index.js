ec32163eca1041f31fa6c0c45df88c5df9cf47c8
Kaic Bento <kaicbento@outlook.com>
2023-06-29 22:48:55 -0300

    chore: delete old code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './scripts/about'
 import './scripts/google-analitycs'
 import { loadTexts, addScrollListener } from './scripts/view'
 import { initI18n } from './scripts/i18n'
-import './scripts/career'
 import { loadTranslateBtn } from './scripts/header'
 
 window.addEventListener('load', async () => {
@@ -19,10 +18,8 @@ if (process.env.NODE_ENV === 'production') {
   if ('serviceWorker' in navigator) {
     console.log('Service worker is registrating.')
     navigator.serviceWorker.register('service-worker.js').then(
-      function () {
-        console.log('Service worker registration complete.')
-      },
-      function () {
+      () => {},
+      () =>  {
         console.log('Service worker registration failure.')
       }
     )
