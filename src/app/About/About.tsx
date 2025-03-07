3d684590a0d263a0704e0b519e77adcb0c431e44
Kaic <kaicbento@outlook.com>
2025-03-07 00:16:16 -0300

    fix: add section on locale and leading loose to text

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -6,8 +6,8 @@ export const About = () => {
   return (
     <article id='about'>
       <div className='grid grid-rows-[20px_1fr_20px] p-8 pb-20 gap-16 sm:p-20'>
-        <h2 className='text-2xl font-bold text-left'>About</h2>
-        <p className='text-base' style={{ whiteSpace: 'pre-line' }}>
+        <h2 className='text-2xl font-bold text-left'>{t('section-name')}</h2>
+        <p className='text-base leading-loose' style={{ whiteSpace: 'pre-line' }}>
           {t('paragraph')}
         </p>
       </div>
