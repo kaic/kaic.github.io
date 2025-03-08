67963e7735790a2d12b56e197d87c958308f8e7c
Kaic <kaicbento@outlook.com>
2025-03-08 17:58:35 -0300

    fix: add github pages required options

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   locales: ['en', 'pt-BR'],
   defaultLocale: 'pt-BR',
   trailingSlash: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '',
 };
 
 export default nextConfig;
