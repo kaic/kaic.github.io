623abc16b19b29e2f62f3411500f2bbd09959dd9
Kaic <kaicbento@outlook.com>
2025-03-08 23:20:46 -0300

    fix: internationalization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import type { Metadata } from 'next';
 import { Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { NextIntlClientProvider } from 'next-intl';
@@ -10,11 +9,6 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
-
-export const metadata: Metadata = {
-  title: 'Kaic',
-  description: 'I develop software for the 21st century',
-};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -3,10 +3,14 @@ import { Main } from '@app//Main/Main';
 import { About } from '@app/About/About';
 import { Contact } from '@app/Contact/contact';
 import { Footer } from '@src/app/Footer/Footer';
+import { useLocale } from 'next-intl';
 
 import { SectionSeparator } from '@app/Components/SectionSeparator';
+import { getStaticProps } from '@src/LanguagueSwitcher';	
 
 export default function Home() {
+  const locale = useLocale();
+  getStaticProps({ locale });
   return (
     <div>
       <Main />
