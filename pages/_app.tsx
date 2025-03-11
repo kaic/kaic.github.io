706a13d7fa9bb969e8cd870d3039fec7bd4aef61
Kaic <kaicbento@outlook.com>
2025-03-11 00:41:51 -0300

    fix: umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,6 @@ import { Source_Code_Pro } from 'next/font/google';
 import './globals.css';
 import { NextIntlClientProvider } from 'next-intl';
 import { useRouter } from 'next/router';
-import Script from 'next/script';
-
-const WEBSITE_ID = '0b493e9f-c3e3-4eb8-a119-4bf453dcec5b';
 
 
 const sourceCodePro = Source_Code_Pro({
@@ -19,12 +16,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        data-website-id={WEBSITE_ID}
-        src="https://cloud.umami.is/script.js"
-      />
       <NextIntlClientProvider
         locale={router.locale || 'en'}
         timeZone='America/Sao_Paulo'
@@ -33,7 +24,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <div className={`scroll-smooth ${sourceCodePro.className} antialiased`}>
           <Component {...pageProps} />
         </div>
-      </NextIntlClientProvider>
-    </>
+    </NextIntlClientProvider>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function MyDocument() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head >
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="0b493e9f-c3e3-4eb8-a119-4bf453dcec5b"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
