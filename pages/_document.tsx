fb8e8f0b73eac050aa0d52c9f96f8edbfdb88b66
Kaic <kaicbento@outlook.com>
2025-03-11 00:51:03 -0300

    fix: add g analytics back

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script'
 
 export default function MyDocument() {
   return (
     <Html lang='en'>
-      <Head >
-        <script defer src="https://cloud.umami.is/script.js" data-website-id="0b493e9f-c3e3-4eb8-a119-4bf453dcec5b"></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-HPDY1JZJ7G`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HPDY1JZJ7G');
+            `,
+          }}
+        />
       </body>
     </Html>
   );
