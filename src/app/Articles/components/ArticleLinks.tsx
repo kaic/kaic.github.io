36d46110595066b454fcffc53ad5dfe260e752ba
Kaic <kaicbento@outlook.com>
2025-03-11 02:22:59 -0300

    feat: add locale to Read link in articles link

--- src/app/Articles/components/ArticleLinks.tsx
+++ src/app/Articles/components/ArticleLinks.tsx
@@ -1,10 +1,13 @@
 import { IArticle } from '../Articles';
+import { useTranslations } from 'next-intl';
 
 interface ArticleLinkProps {
   article: IArticle;
 }
 
 export const ArticleLink = ({ article }: ArticleLinkProps) => {
+  const t = useTranslations('Home.Articles');
+
   return (
     <div className='p-4 text-left block'>
       <h3 className='text-lg font-bold text-white mb-2'>{article.title}</h3>
@@ -19,7 +22,7 @@ export const ArticleLink = ({ article }: ArticleLinkProps) => {
           rel='noopener'
           className='hover:opacity-80 transition-opacity active:text-red-900'
         >
-          Read
+          {t(`link`)}
         </a>
       </div>
     </div>
