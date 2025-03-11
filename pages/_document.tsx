973f1964f07b876fddc3be03b24efc3d81990b2c
Kaic <kaicbento@outlook.com>
2025-03-11 01:15:29 -0300

    fix: AGAIN google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,31 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { GoogleAnalytics } from '@next/third-parties/google'; 
+
+const GA_TRACKING_ID = 'G-QQH756KNV';
 
 export default function MyDocument() {
   return (
     <Html lang='en'>
       <Head />
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
       </body>
-      <GoogleAnalytics gaId="G-QQH756KNV" />
     </Html>
   );
 }
