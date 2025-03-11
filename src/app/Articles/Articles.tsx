4bd7a6fb72cb7bb376cdce7de66c259584c9966e
Kaic <kaicbento@outlook.com>
2025-03-11 03:29:10 -0300

    fix? subscribe link

--- src/app/Articles/Articles.tsx
+++ src/app/Articles/Articles.tsx
@@ -49,7 +49,7 @@ function Articles() {
                 <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
               </svg>
               <span className="font-medium">
-                {t(`link`)}
+                {t(`subscribe-link`)}
               </span>
             </a>
           </div>
